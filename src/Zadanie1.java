3cef57a20d3c88cb800b693b3fc961946ae91b1c
kosty <round.mc@yandex.by>
2023-12-17 13:14:42 -0500

    Task 1 исправил

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -4,22 +4,15 @@ import java.util.regex.Pattern;
 public class Zadanie1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
 
-        while (true) {
-            System.out.println("Введите строку (для выхода введите 'exit'):");
-            String input = scanner.nextLine();
-
-            if (input.equals("exit")) {
-                break;
-            }
-
-            String[] words = input.split(" ");
-
-            for (String word : words) {
-                if (word.length() >= 2 && word.length() <= 6 && Pattern.matches("^[\\p{Lu}]+$", word)) {
-                    System.out.println("Аббревиатура: " + word);
-                }
+        for (String word : words) {
+            if (word.length() >= 2 && word.length() <= 6 && Pattern.matches("^[\\p{Lu}]+$", word)) {
+                System.out.println( " Аббревиатура: " + word);
             }
         }
     }
 }
+
